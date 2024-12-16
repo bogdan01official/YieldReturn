4d7873100e399dc3ad653b0aa70996bdeab6cb0f
bogdan01official <108180181+bogdan01official@users.noreply.github.com>
2024-12-16 22:45:40 +0500

    Add files via upload

---
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        private readonly NumberGeherateYield _numberGeneratorYield;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _numberGeneratorYield = new NumberGeherateYield();
+        }
+
+        private async void GenerateButtonClick(object sender, RoutedEventArgs e)
+        {
+            NumbersListBox.Items.Clear();
+            ProgressBar.Value = 0;
+
+            if (int.TryParse(StartTextBox.Text, out int start) && int.TryParse(EndTextBox.Text, out int end))
+            {
+                int totalNumner = end - start + 1;
+                ProgressBar.Maximum = totalNumner;
+
+                Thread thread = new Thread(() =>
+                {
+                    foreach (var number in _numberGeneratorYield.GenerateNumbers(start, end))
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            NumbersListBox.Items.Add(number);
+                            ProgressBar.Value++;
+                        });
+                    }
+                    Dispatcher.Invoke(() => MessageBox.Show("Генерация завершена!"));
+                });
+                thread.Start();
+
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числа.");
+            }
+        }
+
+    }
+}
---
+++ WpfApp2/WpfApp2/NumberGeherateYield.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    internal class NumberGeherateYield
+    {
+
+        public IEnumerable<int> GenerateNumbers(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Thread.Sleep(500);
+                yield return i;
+            }
+        }
+
+    }
+}
